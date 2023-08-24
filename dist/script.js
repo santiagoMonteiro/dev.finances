9f310ac7247dd3cad66c11badb33575fb30739d8
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2023-08-24 17:19:52 -0400

    comeback

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,9 @@
 const Modal = {
   open() {
-    document.querySelector(".modal-overlay").classList.add("active");
+    document.querySelector("#modal-overlay").classList.add("active");
   },
   close() {
-    document.querySelector(".modal-overlay").classList.remove("active");
+    document.querySelector("#modal-overlay").classList.remove("active");
   },
 };
 
